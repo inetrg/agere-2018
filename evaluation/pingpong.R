9718587f6a483e8420a4059cc8a39e0e2c1c8c4e
Joseph Noir <joseph.noir@gmail.com>
2018-08-14 17:30:35 +0200

    Add data and eval script for pingpong benchmark

---
+++ evaluation/pingpong.R
@@ -0,0 +1,19 @@
+times <- read.csv("evaluation/pingpong.csv", sep = ",", as.is = c(numeric, numeric, numeric, numeric, numeric, numeric, numeric, numeric, numeric, numeric, numeric))
+df <- times[-c(1), ]
+df$avg <- rowMeans(df[,2:11])
+df$sdev <- apply(df[,2:11], 1, sd)
+df$upper_percentile <- apply(df[,2:11], 1, quantile, probs=c(0.95))
+df$lower_percentile <- apply(df[,2:11], 1, quantile, probs=c(0.05))
+pp_plot <- ggplot(df, aes(x=loss.percentage, y=avg / 1000)) +
+           geom_line() +
+           geom_errorbar(data=df, mapping=aes(x=loss.percentage, ymin=lower_percentile / 1000, ymax=upper_percentile / 1000), width=0.2) + # , size=0.3
+           scale_x_continuous(breaks = seq(0, 10, 1)) + # expand = c(0, 0), limits = c(0, 10)
+           scale_y_continuous(limits = c(0, 62), breaks = seq(0, 60, 10)) + # expand = c(0, 0), limits = c(0, 10)
+           theme_bw() +
+           theme(
+             legend.title = element_blank(),
+             legend.position = "top",
+             text=element_text(size=9)
+           ) +
+           labs(x="Packet Loss [%]", y="Time [s]")
+ggsave("pingpong.pdf", plot=pp_plot, width=3.4, height=2.3)
