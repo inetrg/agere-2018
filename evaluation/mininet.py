00be1069b9a8a464cf982e6ae4b4013e4d453323
Joseph Noir <joseph.noir@gmail.com>
2018-09-07 17:53:03 +0200

    Some updates, still doesn't work

--- evaluation/mininet.py
+++ evaluation/mininet.py
@@ -4,6 +4,7 @@ mininet_path = '/usr/local/lib/python2.7/dist-packages/mininet-2.2.2-py2.7.egg'
 
 import time
 import sys
+import argparse
 
 sys.path.insert(0, mininet_path)
 
@@ -13,6 +14,7 @@ from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.node import CPULimitedHost
 from mininet.util import pmonitor
+from time import time
 
 from time import sleep
 
@@ -47,91 +49,119 @@ def set_interface_delay(host, action, interface, delay): #action: add/change/del
         command = 'tc qdisc {} dev {} root netem delay {}ms {}ms distribution {}'.format(action, interface, delay, variance, distribution)
     else:
         command = 'tc qdisc {} dev {} root netem delay {}ms'.format(action, interface, delay)
-    print('running "{}"'.format(command))
+    print('{} -> "{}"'.format(host.name, command))
     host.cmd(command)
 
 def set_interface_packet_lost(host, action, interface, packet_loss): #action: add/change/delete
     # command = 'tc qdisc {} dev {} parent 1:1 netem loss {}%'.format(action, interface, packet_loss)
     command = 'tc qdisc {} dev {} root netem loss {}%'.format(action, interface, packet_loss)
-    print('running: "{}"').format(command)
+    print('{} -> "{}"').format(host.name, command)
     host.cmd(command)
 
 def set_loss(host, packet_loss):
     iface = '{}-eth0'.format(host.name)
     op = 'add' if packet_loss > 0 else 'del'
-    print('setting loss on "{}" to "{}%" using "{}"'.format(iface, packet_loss, op))
     set_interface_packet_lost(host, op, iface, packet_loss)
 
 def set_delay(host, delay):
     iface = '{}-eth0'.format(host.name)
     op = 'add' if delay > 0 else 'del'
-    print('setting delay on "{}" to "{}ms" using "{}"'.format(iface, delay, op))
     set_interface_delay(host, op, iface, delay)
 
 def get_info(host):
     iface = '{}-eth0'.format(host.name)
     command = 'tc qdisc show  dev {}'.format(iface)
-    print('running: "{}"'.format(command))
+    print('{} -> "{}"'.format(host.name, command))
     host.cmdPrint(command)
 
 def set_min_rto(host, timeout):
     command = '10.0.0.0/8 dev {}-eth0 proto kernel scope link src {} rto_min {}ms'.format(host.name, host.IP(), timeout)
-    print('running: "{}"'.format(command))
+    print('{} -> "{}"'.format(host.name, command))
     host.cmdPrint(command)
 
 def main():
-    delay = 0
-    for loss in range(0, 11):
-        for run in range(10):
-            print(">> Loss = {}%, run {}".format(loss, run))
-            net = Mininet(topo = TwoHostsTopology(ls=loss), link=TCLink, host=CPULimitedHost)
-            net.start()
+    parser = argparse.ArgumentParser(description='CAF newbs on Mininet.')
+    parser.add_argument('-l', '--loss',    help='', type=int, default=0)
+    parser.add_argument('-d', '--delay',   help='', type=int, default=0)
+    parser.add_argument('-r', '--runs',    help='', type=int, default=1)
+    parser.add_argument('-T', '--threads', help='', type=int, default=1)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-t', '--tcp', help='', action='store_true')
+    group.add_argument('-u', '--udp', help='', action='store_true')
+    args = vars(parser.parse_args())
+    run = 0
+    max_runs = args['runs']
+    while run < max_runs:
+        loss = args['loss']
+        delay = args['delay']
+        print(">> Run {} with {}% loss and {}ms delay".format(run, loss, delay))
+        net = Mininet(topo = TwoHostsTopology(ls=loss), link=TCLink, host=CPULimitedHost)
+        net.start()
 
-            print("configuring hosts")
-            h1 = net.get('h1')
-            set_delay(h1, delay)
-            set_loss(h1, loss)
-            set_min_rto(h1, 40)
-            # get_info(h1) # does not print anything ...
-            h2 = net.get('h2')
-            set_delay(h2, delay)
-            set_loss(h2, loss)
-            set_min_rto(h2, 40)
-            # get_info(h2)
+        print("Configuring hosts ...")
+        # host 1
+        h1 = net.get('h1')
+        set_delay(h1, delay)
+        set_loss(h1, loss)
+        set_min_rto(h1, 40)
+        # host 2
+        h2 = net.get('h2')
+        set_delay(h2, delay)
+        set_loss(h2, loss)
+        set_min_rto(h2, 40)
 
-            files = {}
-            file_path = '/home/localadmin/logs/reliable-udp' # was /tmp
-            servererr = open('{}/ppserver-{}-{}.err'.format(file_path, loss, run), 'w')
-            serverout = open('{}/ppserver-{}-{}.out'.format(file_path, loss, run), 'w')
-            clienterr = open('{}/ppclient-{}-{}.err'.format(file_path, loss, run), 'w')
-            clientout = open('{}/ppclient-{}-{}.out'.format(file_path, loss, run), 'w')
+        files = {}
+        # servererr = './server-{}-{}-{}.err'.format(loss, delay, run)
+        # serverout = './server-{}-{}-{}.out'.format(loss, delay, run)
+        # clienterr = './client-{}-{}-{}.err'.format(loss, delay, run)
+        # clientout = './client-{}-{}-{}.out'.format(loss, delay, run)
+        servererr = open('./server-{}-{}-{}.err'.format(loss, delay, run), 'w')
+        serverout = open('./server-{}-{}-{}.out'.format(loss, delay, run), 'w')
+        clienterr = open('./client-{}-{}-{}.err'.format(loss, delay, run), 'w')
+        clientout = open('./client-{}-{}-{}.out'.format(loss, delay, run), 'w')
 
-            caf_opts = '--scheduler.max-threads=1'
+        caf_opts = '--scheduler.max-threads={}'.format(args['threads'])
+        prog = 'pingpong'
+        if args['tcp']:
+            prog = 'pingpong_tcp'
 
-            print("starting server")
-            servercommand = '../build/bin/pingpong_tcp -s --host=\\"{}\\" {}'.format(h1.IP(), caf_opts)
-            print(servercommand)
-            p1 = h1.popen(servercommand, shell=True, universal_newlines=True, stdout=serverout, stderr=servererr) # not sure if this shouldn't be h1.cmd
+        print("Starting server")
+        servercommand = '../build/bin/{} -s --host=\\"{}\\" {} '.format(prog, h1.IP(), caf_opts)
+        print('> {}'.format(servercommand))
+        # h1.cmdPrint(servercommand)
+        # p1 = int(h1.cmd('echo $!'))
+        p1 = h1.popen(servercommand, shell=True, universal_newlines=True, stdout=serverout, stderr=servererr)
 
-            print("starting client")
-            clientcommand = '../build/bin/pingpong_tcp -m 2000 --host=\\"{}\\" {}'.format(h1.IP(), caf_opts)
-            print(clientcommand)
-            p2 = h2.popen(clientcommand, shell=True, universal_newlines=True, stdout=clientout, stderr=clienterr) # not sure if this shouldn't be h1.cmd
+        print("Starting client")
+        clientcommand = '../build/bin/{} -m 2000 --host=\\"{}\\" {}'.format(prog, h1.IP(), caf_opts)
+        print('> {}'.format(clientcommand))
+        # h2.cmdPrint(clientcommand)
+        # p2 = int(h2.cmd('echo $!'))
+        p2 = h2.popen(clientcommand, shell=True, universal_newlines=True, stdout=clientout, stderr=clienterr)
+        # end_time = time() + 150
+        # for line in h2.monitor():
+            # print '%s: %s'.format(h2.name, line)
+            # if time() >= end_time:
+                # for p in processes.values():
+                    # p.send_signal(SIGINT)
 
-            # CLI(net)
-            p2.wait()
-            p1.kill()
-            servererr.close()
-            serverout.close()
-            clienterr.close()
-            clientout.close()
-            h1.cmd('kill %pingpong')
-            h1.cmd('killall pingpong')
-            h2.cmd('kill %pingpong')
-            h2.cmd('killall pingpong')
-            sleep(1)
-            net.stop()
-            sleep(1)
+        # h2.cmd('wait', p2)
+        # h1.cmd('kill', p1)
+        # CLI(net)
+        p2.wait()
+        p1.kill()
+        servererr.close()
+        serverout.close()
+        clienterr.close()
+        clientout.close()
+        h1.cmd('kill %{}'.format(prog))
+        h1.cmd('killall {}'.format(prog))
+        h2.cmd('kill %{}'.format(prog))
+        h2.cmd('killall {}'.format(prog))
+        # sleep(1)
+        net.stop()
+        # sleep(1)
+        run += 1
 
 if __name__ == '__main__':
     main()
