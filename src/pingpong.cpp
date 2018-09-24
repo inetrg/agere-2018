1655f71681d4c498b2be2b3d33ce12f67d3eb7b0
Joseph Noir <joseph.noir@gmail.com>
2018-09-24 08:46:02 -0700

    Drop timeouts for UDP, don't want to measure them

--- src/pingpong.cpp
+++ src/pingpong.cpp
@@ -34,6 +34,9 @@ struct state {
 
 behavior raw_server(stateful_newb<new_raw_msg, state>* self, actor responder) {
   self->state.responder = responder;
+  self->set_timeout_handler([&](timeout_msg&) {
+    // Drop timeouts.
+  });
   return {
     [=](new_raw_msg& msg) {
       uint32_t counter;
@@ -64,6 +67,9 @@ behavior raw_server(stateful_newb<new_raw_msg, state>* self, actor responder) {
 
 behavior raw_client(stateful_newb<new_raw_msg, state>* self) {
   std::cout << "client is running" << std::endl;
+  self->set_timeout_handler([&](timeout_msg&) {
+    // Drop timeouts.
+  });
   return {
     [=](start_atom, size_t messages, actor responder) {
       std::cout << "starting" << std::endl;
