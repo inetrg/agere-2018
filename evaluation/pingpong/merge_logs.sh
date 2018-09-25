071aa78d3decdf39bf4769a948515fac43a4e50e
None <None>
2018-09-25 15:03:45 -0700

    Add ping pong results

--- evaluation/pingpong/merge_logs.sh
+++ evaluation/pingpong/merge_logs.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
+delay=0
 
-rm reliable_udp.csv
-echo "loss, value0, value1, value2, value3, value4, value5, value6, value7, value8, value9" >> reliable_udp.csv
+i_arg="''"
+if [ "$(uname)" == "Darwin" ]; then
+  i_arg="''"
+elif [ "$(uname)" == "FreeBSD" ]; then
+  i_arg="''"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  i_arg=""
+fi
+
+
+file="reliable_udp-${delay}.csv"
+rm $file
+echo "loss, value0, value1, value2, value3, value4, value5, value6, value7, value8, value9" >> $file
 for i in {0..10}
 do
-  echo "$i,$(paste -d ',' logs/reliable-udp/ppclient-$i-*.out)" >> reliable_udp.csv
+echo "$i,$(paste -d ',' udp-client-$i-${delay}-*.out)" >> $file
 done
-sed -i '' 's/ms//g' reliable_udp.csv
+sed -i $i_arg 's/ms//g' "$file" 
 
-rm tcp.csv
-echo "loss, value0, value1, value2, value3, value4, value5, value6, value7, value8, value9" >> tcp.csv
+file="reliable_ordered_udp-${delay}.csv"
+rm $file
+echo "loss, value0, value1, value2, value3, value4, value5, value6, value7, value8, value9" >> $file
 for i in {0..10}
 do
-  echo "$i,$(paste -d ',' logs/tcp/ppclient-$i-*.out)" >> tcp.csv
+echo "$i,$(paste -d ',' udp-ordered-client-$i-${delay}-*.out)" >> $file
+done
+sed -i $i_arg 's/ms//g' "$file"
+
+file="tcp-${delay}.csv"
+rm $file
+echo "loss, value0, value1, value2, value3, value4, value5, value6, value7, value8, value9" >> $file
+for i in {0..10}
+do
+  echo "$i,$(paste -d ',' tcp-client-$i-${delay}-*.out)" >> $file
 done
 
-sed -i '' 's/ms//g' tcp.csv
+sed -i $i_arg 's/ms//g' "$file"
