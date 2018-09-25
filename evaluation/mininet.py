89c43d0d771c0bfd1c18d3a6ba5e49c783bc98d0
None <None>
2018-09-25 14:50:29 -0700

    Update miniet script to working version

--- evaluation/mininet.py
+++ evaluation/mininet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-#mininet_path = '/usr/local/lib/python2.7/dist-packages/mininet-2.2.2-py2.7.egg'
-mininet_path = '/usr/local/lib/python2.7/dist-packages/mininet-2.3.0d4-py2.7.egg'
+mininet_path = '/usr/local/lib/python2.7/dist-packages/mininet-2.2.2-py2.7.egg'
+#mininet_path = '/usr/local/lib/python2.7/dist-packages/mininet-2.3.0d4-py2.7.egg'
 
 import time
 import sys
@@ -22,9 +22,8 @@ from time import sleep
 class TwoHostsTopology(Topo):
     "peer to peer topology"
 
-    def build(self, ls=0, dy='10ms'):
+    def build(self):
         "Build two nodes with a direct link"
-        #print('loss = %d, delay = %s' % (ls, dy))
 
         # Add two hosts.
         lhs = self.addHost('h1', cpu=.5)
@@ -32,12 +31,6 @@ class TwoHostsTopology(Topo):
 
         # Add direct link.
         self.addLink(lhs, rhs)
-        #if ls != 0:
-        #    linkopts = dict(bw=100, delay=dy, loss=ls) #, use_htb=True)
-        #    self.addLink(lhs, rhs, **linkopts)
-        #else:
-        #    linkopts = dict(bw=100, delay=dy) #, use_htb=True)
-        #    self.addLink(lhs, rhs, **linkopts)
 
 topos = { 'p2p': ( lambda: TwoHostsTopology() ) }
 
@@ -76,7 +69,8 @@ def get_info(host):
     host.cmdPrint(command)
 
 def set_min_rto(host, timeout):
-    command = '10.0.0.0/8 dev {}-eth0 proto kernel scope link src {} rto_min {}ms'.format(host.name, host.IP(), timeout)
+    " Change min_rto for a route on Linux, might require the 'ms' ...
+    command = 'ip route change 10.0.0.0/8 dev {}-eth0 proto kernel scope link src {} rto_min {}'.format(host.name, host.IP(), timeout)
     print('{} -> "{}"'.format(host.name, command))
     host.cmdPrint(command)
 
@@ -86,10 +80,12 @@ def main():
     parser.add_argument('-d', '--delay',   help='', type=int, default=0)
     parser.add_argument('-r', '--runs',    help='', type=int, default=1)
     parser.add_argument('-T', '--threads', help='', type=int, default=1)
-    parser.add_argument('-o', '--rto',     help='', type=int, default=40)
+    parser.add_argument('-R', '--rto',     help='', type=int, default=40)
+    parser.add_argument('-o', '--ordered', help='', action='store_true')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-t', '--tcp', help='', action='store_true')
-    group.add_argument('-u', '--udp', help='', action='store_true')
+    group.add_argument('-t', '--tcp',  help='', action='store_true')
+    group.add_argument('-u', '--udp',  help='', action='store_true')
+    group.add_argument('-q', '--quic', help='', action='store_true')
     args = vars(parser.parse_args())
     run = 0
     max_runs = args['runs']
@@ -98,11 +94,18 @@ def main():
         loss = args['loss']
         delay = args['delay']
         min_rto = args['rto']
-        proto = 'udp'
-        if is_tcp:
+        proto = ''
+        if args['tcp']:
             proto = 'tcp'
+        elif args['udp']:
+            if args['ordered']:
+                proto = 'udp-ordered'
+            else:
+                proto = 'udp'
+        elif args['quic']:
+            proto = 'quic'
         print(">> Run {} with {}% loss and {}ms delay".format(run, loss, delay))
-        net = Mininet(topo = TwoHostsTopology(ls=loss), link=TCLink, host=CPULimitedHost)
+        net = Mininet(topo = TwoHostsTopology(), link=TCLink, host=CPULimitedHost)
         net.start()
 
         print("Configuring hosts ...")
@@ -119,27 +122,31 @@ def main():
         if is_tcp:
             set_min_rto(h2, min_rto)
 
-        files = {}
-        # servererr = './server-{}-{}-{}.err'.format(loss, delay, run)
-        # serverout = './server-{}-{}-{}.out'.format(loss, delay, run)
-        # clienterr = './client-{}-{}-{}.err'.format(loss, delay, run)
-        # clientout = './client-{}-{}-{}.out'.format(loss, delay, run)
-        servererr = open('./{}-server-{}-{}-{}.err'.format(proto, loss, delay, run), 'w')
-        serverout = open('./{}-server-{}-{}-{}.out'.format(proto, loss, delay, run), 'w')
-        clienterr = open('./{}-client-{}-{}-{}.err'.format(proto, loss, delay, run), 'w')
-        clientout = open('./{}-client-{}-{}-{}.out'.format(proto, loss, delay, run), 'w')
+        servererr = open('./pingpong/{}-server-{}-{}-{}.err'.format(proto, loss, delay, run), 'w')
+        serverout = open('./pingpong/{}-server-{}-{}-{}.out'.format(proto, loss, delay, run), 'w')
+        clienterr = open('./pingpong/{}-client-{}-{}-{}.err'.format(proto, loss, delay, run), 'w')
+        clientout = open('./pingpong/{}-client-{}-{}-{}.out'.format(proto, loss, delay, run), 'w')
 
         caf_opts = '--scheduler.max-threads={}'.format(args['threads'])
-        prog = 'pingpong'
+        prog = ''
         if args['tcp']:
             prog = 'pingpong_tcp'
+	elif args['udp']:
+            prog = 'pingpong_udp'
+            if args['ordered']:
+                caf_opts = '{} --ordered'.format(caf_opts)
+        elif args['quic']:
+            prog = 'pingpong_quic'
 
         print("Starting server")
-        servercommand = '../build/bin/{} -s --host=\\"{}\\" {} '.format(prog, h1.IP(), caf_opts)
+        servercommand = '../build/bin/{} -s {} '.format(prog, caf_opts)
         print('> {}'.format(servercommand))
         # h1.cmdPrint(servercommand)
         # p1 = int(h1.cmd('echo $!'))
         p1 = h1.popen(servercommand, shell=True, universal_newlines=True, stdout=serverout, stderr=servererr)
+
+        # Give everything a bit of time to start.
+        sleep(1)
 
         print("Starting client")
         clientcommand = '../build/bin/{} -m 2000 --host=\\"{}\\" {}'.format(prog, h1.IP(), caf_opts)
@@ -147,15 +154,7 @@ def main():
         # h2.cmdPrint(clientcommand)
         # p2 = int(h2.cmd('echo $!'))
         p2 = h2.popen(clientcommand, shell=True, universal_newlines=True, stdout=clientout, stderr=clienterr)
-        # end_time = time() + 150
-        # for line in h2.monitor():
-            # print '%s: %s'.format(h2.name, line)
-            # if time() >= end_time:
-                # for p in processes.values():
-                    # p.send_signal(SIGINT)
 
-        # h2.cmd('wait', p2)
-        # h1.cmd('kill', p1)
         # CLI(net)
         p2.wait()
         p1.kill()
